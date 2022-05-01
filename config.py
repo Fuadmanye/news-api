7e83ee74bea30b4cfaaec8be4e28f1f7f632f888
fuad manye / <fahadbasi333@gmail.com>
2022-05-01 15:55:42 +0300

    add the parent class

---
+++ config.py
@@ -0,0 +1,43 @@
+import os
+class Config:
+    '''
+    General configuration parent class
+    '''
+    # https://newsapi.org/v2/sources?apiKey=f1ffdc1e372b4ca09f0a7c5abe5a0194
+    # https://newsapi.org/v2/top-headlines?country=us&apiKey=f1ffdc1e372b4ca09f0a7c5abe5a0194
+    # https://newsapi.org/v2/everything?q=bitcoin&apiKey=f1ffdc1e372b4ca09f0a7c5abe5a0194
+
+    NEWS_SOURCES_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    ARTICLES_BASE_URL = 'https://newsapi.org/v2/everything?sources={}&apiKey={}'
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    
+    @staticmethod
+    def init_app(app):
+            pass
+
+class ProdConfig(Config):
+    '''articles_result = Articles(
+                id, author, title, description, url, image, date)
+        articles_object.append(articles_result)
+    Production  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+
+}  
